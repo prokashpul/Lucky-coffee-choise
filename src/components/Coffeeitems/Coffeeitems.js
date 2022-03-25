ec9a8495387367ed3be6ff3df83d0bb7f3670696
prokash <pprokash102@gmail.com>
2022-03-25 22:28:14 +0600

    jsone data load to useState and useEffect and use map

---
+++ src/components/Coffeeitems/Coffeeitems.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "./Coffeeitems.css";
+const Coffeeitems = (props) => {
+  return <div>item</div>;
+};
+
+export default Coffeeitems;
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Coffeeitems from "../Coffeeitems/Coffeeitems";
 import "./Shop.css";
 const Shop = () => {
+  // useState and useEffect use and data load
+  const [coffeeItems, setCoffeItems] = useState([]);
+  useEffect(() => {
+    fetch("coffeedata.json")
+      .then((res) => res.json())
+      .then((data) => setCoffeItems(data));
+  }, []);
   return (
     <section className="shop">
-      <div className="coffee-items">Hello</div>
+      <div className="coffee-items">
+        {coffeeItems?.map((coffee) => (
+          <Coffeeitems></Coffeeitems>
+        ))}
+      </div>
       <div className="cart-details">Selected Coffees</div>
     </section>
   );
