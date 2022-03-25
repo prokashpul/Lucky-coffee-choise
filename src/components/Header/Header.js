81ea79b723df092d6d475143024001e258c1eec1
prokash <pprokash102@gmail.com>
2022-03-25 21:55:19 +0600

    header section add and show display

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Header.css";
+const Header = () => {
+  return (
+    <header className="container header">
+      <h2>Lucky Coffee</h2>
+      <h4>Choose 4 Coffee</h4>
+    </header>
+  );
+};
+
+export default Header;
